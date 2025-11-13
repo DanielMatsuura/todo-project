50bab13ae4980d7e87c00cb67899dff68f424617
TadeoMatsuura <85190874+DanielMatsuura@users.noreply.github.com>
2025-11-13 15:00:08 -0300

    Setting Docker
    - Added Dockerfile for backend
    - Added Dockerfile for frontend
    - Added docker-compose.yml to run frontend, backend, and MongoDB
    - Configured ports and .env support for both services
    - Ensures containers can communicate and be accessed via localhost

--- front-end-todo/vite.config.ts
+++ front-end-todo/vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: Number(process.env.VITE_PORT) || 5174
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
